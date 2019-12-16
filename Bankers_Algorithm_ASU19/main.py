2fc07c2c2a6186e06a328d36e1749162d6e9384e
TheDigitalPhoenixX <mohamed.xyz@hotmail.com>
2019-12-16 03:06:06 +0200

    refactor: main function

--- Bankers_Algorithm_ASU19/main.py
+++ Bankers_Algorithm_ASU19/main.py
@@ -1,25 +1,31 @@
-def main(numOfProcess, numOfResources, available, allocation, maxResources):
+def allEqualTrue(inputList):
+    for item in inputList:
+        if not item:
+            return False
+    return True
+
+def isCurrentHigherThanNeed(current, need):
+    for j in range(len(need)):
+        if need[j] > current[j]:
+            return False
+    return True
+
+def isInSafeState(numOfProcess, numOfResources, available, allocation, maxResources):
     need = [[maxResources[i][j]-allocation[i][j] for j in range(numOfResources)] for i in range(numOfProcess)]
     currentAvailable = available.copy()
     finished = [False for i in range(numOfProcess)]
 
-    canDoWork = True
-    while canDoWork:
-        canDoWork = False
+    availableIncreased = True
+    while availableIncreased:
+        availableIncreased = False
         for i in range(numOfProcess):
-
-            isNeedLowerThanCurrent = True
-            for j in range(numOfResources):
-                isNeedLowerThanCurrent = (need[i][j] < currentAvailable[j]) and isNeedLowerThanCurrent
-
-            if not finished[i] and isNeedLowerThanCurrent:
+            if not finished[i] and isCurrentHigherThanNeed(currentAvailable, need[i]):
                 for j in range(numOfResources):
                     currentAvailable[j] += allocation[i][j]
-                    finished[i] = True
-                canDoWork = True
+                finished[i] = True
+                availableIncreased = True
 
-    safeState = True
-    for isFinished in finished:
-        safeState = isFinished and safeState
+    return allEqualTrue(finished)
 
-    print(safeState)
+def main(numOfProcess, numOfResources, available, allocation, maxResources):
+    print(isInSafeState(numOfProcess, numOfResources, available, allocation, maxResources))
