1d9652bc8e13a6230377a5704a4accf9aa2e4253
TheDigitalPhoenixX <mohamed.xyz@hotmail.com>
2019-12-16 20:08:22 +0200

    feat: Handle Deadlocks

--- Bankers_Algorithm_ASU19/main.py
+++ Bankers_Algorithm_ASU19/main.py
@@ -3,11 +3,13 @@ class NoSafeSeqFoundException(Exception):
         self.processesFinished = processesFinished
         self.availableResources = availableResources
 
+
 def allEqualTrue(inputList):
     for item in inputList:
         if not item:
             return False
     return True
+
 
 def isCurrentHigherThanNeed(current, need):
     for j in range(len(need)):
@@ -15,8 +17,12 @@ def isCurrentHigherThanNeed(current, need):
             return False
     return True
 
+
 def getSafeSeq(numOfProcess, numOfResources, available, allocation, maxResources):
-    need = [[maxResources[i][j]-allocation[i][j] for j in range(numOfResources)] for i in range(numOfProcess)]
+    need = [
+        [maxResources[i][j] - allocation[i][j] for j in range(numOfResources)]
+        for i in range(numOfProcess)
+    ]
     currentAvailable = available.copy()
     finished = [False for i in range(numOfProcess)]
     SafeSeq = []
@@ -35,3 +41,45 @@ def getSafeSeq(numOfProcess, numOfResources, available, allocation, maxResources
         return SafeSeq
     else:
         raise NoSafeSeqFoundException(SafeSeq, currentAvailable)
+
+
+def handleDeadlock(numOfProcess, numOfResources, available, allocation, maxResources):
+    processesTerminated = []
+    inUnsafeState = True
+    while inUnsafeState:
+        try:
+            getSafeSeq(
+                numOfProcess, numOfResources, available, allocation, maxResources
+            )
+        except NoSafeSeqFoundException as ex:
+            numOfProcess -= len(ex.processesFinished)
+            allocation = [
+                process
+                for i, process in enumerate(allocation)
+                if i not in ex.processesFinished
+            ]
+            maxResources = [
+                process
+                for i, process in enumerate(maxResources)
+                if i not in ex.processesFinished
+            ]
+            available = ex.availableResources
+
+            HighestAllocation = 0
+            HighestAllocationSum = sum(allocation[0])
+            for i, alloc in enumerate(allocation):
+                allocSum = sum(alloc)
+                if allocSum > HighestAllocationSum:
+                    HighestAllocationSum = allocSum
+                    HighestAllocation = i
+
+            processesTerminated.append(HighestAllocation)
+
+            for i in range(len(allocation)):
+                available[i] += allocation[HighestAllocation][i]
+            numOfProcess -= 1
+            del allocation[HighestAllocation]
+            del maxResources[HighestAllocation]
+        else:
+            inUnsafeState = False
+    return processesTerminated
